a5f566107b7feeb4fce55b54b4002232c9850303
ojembajoy <ojembankechi.beautyemperum@gmail.com>
2025-09-02 09:09:32 -0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./Footer.module.css";
 
+
+// updated footer
 const Footer = () => {
     return (
-        <footer className={styles.footer}>
+        <footer className={styles.footer}> 
             <p>&copy; 2025 W3Schools Clone. All rights reserved.</p>
             <div className={styles.links}>
                 <a href="#">Privacy</a>
